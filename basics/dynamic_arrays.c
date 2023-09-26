a81eb3786660f0712ac0c703014f9282e8a17861
hitogava <hitogavax@yandex.ru>
2023-09-26 10:29:22 +0700

    findSubArr function

--- basics/dynamic_arrays.c
+++ basics/dynamic_arrays.c
@@ -24,6 +24,20 @@ int* concatArr(int* arr1, size_t len1, int* arr2, size_t len2, size_t* resLen) {
     return resArr;
 }
 
+int* findSubArr (int* arr1, size_t len1, int* arr2, size_t len2) {
+    for (size_t i = 0; i <= (len1 - len2); i++) {
+        size_t j = 0;
+        if (arr1[i] == arr2[j]) {
+            for (; j < len2 && (arr1[i+j] == arr2[j]); j++) ;
+            if (j == len2) { 
+                printf("i: %zu j: %zu\n", i, j);
+                return &arr1[i];
+            }
+        }
+    }
+    return NULL;
+}
+
 int main (void) {
     size_t len1;
     size_t len2;
@@ -38,8 +52,14 @@ int main (void) {
     printArr(arr1, len1);
     printArr(arr2, len2);
 
-    size_t len3;
-    int* arr3 = concatArr(arr1, len1, arr2, len2, &len3);
-    printArr(arr3, len3);
+    // size_t len3;
+    // int* arr3 = concatArr(arr1, len1, arr2, len2, &len3);
+    // printArr(arr3, len3);
+    int* subArr = findSubArr(arr1, len1, arr2, len2);
+    if (subArr) {
+        printf("%p %d\n", subArr, *subArr);
+    } else {
+        puts ("NULL");
+    }
     return 0;
 }
