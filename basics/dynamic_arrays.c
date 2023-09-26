229d37a57b9d3a2f4c43316bbfe54423f40f663b
hitogava <hitogavax@yandex.ru>
2023-09-26 10:14:50 +0700

    concatArray funcion

--- basics/dynamic_arrays.c
+++ basics/dynamic_arrays.c
@@ -14,14 +14,32 @@ void printArr (int* array, size_t len) {
     printf("\n");
 }
 
+int* concatArr(int* arr1, size_t len1, int* arr2, size_t len2, size_t* resLen) {
+    int* resArr = malloc (sizeof(int) * (len1 + len2));
+    // null checking
+    size_t currInd = 0;
+    for (size_t i = 0; i < len1; resArr[currInd++] = arr1[i++]) ;
+    for (size_t i = 0; i < len2; resArr[currInd++] = arr2[i++]) ;
+    *resLen = currInd;
+    return resArr;
+}
+
 int main (void) {
-    size_t len1 = 7;
-    size_t len2 = 2;
+    size_t len1;
+    size_t len2;
+    scanf("%zu", &len1);
+    scanf("%zu", &len2);
     int* arr1 = malloc(sizeof(int) * len1);
     int* arr2 = malloc(sizeof(int) * len2);
+
     scanArr(arr1, len1);
     scanArr(arr2, len2);
+
     printArr(arr1, len1);
     printArr(arr2, len2);
+
+    size_t len3;
+    int* arr3 = concatArr(arr1, len1, arr2, len2, &len3);
+    printArr(arr3, len3);
     return 0;
 }
