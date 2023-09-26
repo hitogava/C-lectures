5da217488369155251149a08ece231847b385922
hitogava <hitogavax@yandex.ru>
2023-09-26 16:11:30 +0700

    Finally working removeSubArr

--- basics/dynamic_arrays.c
+++ basics/dynamic_arrays.c
@@ -14,8 +14,16 @@ void printArr (int* array, size_t len) {
     printf("\n");
 }
 
+void nullCheck(void* ptr) {
+    if (!ptr) {
+        puts ("Memory allocation error");
+        exit(0);
+    }
+}
+
 int* concatArr(int* arr1, size_t len1, int* arr2, size_t len2, size_t* resLen) {
     int* resArr = malloc (sizeof(int) * (len1 + len2));
+    nullCheck(resArr);
     // null checking
     size_t currInd = 0;
     for (size_t i = 0; i < len1; resArr[currInd++] = arr1[i++]) ;
@@ -31,7 +39,6 @@ int* findSubArr (int* arr1, size_t len1, int* arr2, size_t len2) {
         if (arr1[i] == arr2[j]) {
             for (; j < len2 && (arr1[i+j] == arr2[j]); j++) ;
             if (j == len2) { 
-                printf("i: %zu j: %zu\n", i, j);
                 return &arr1[i];
             }
         }
@@ -40,18 +47,18 @@ int* findSubArr (int* arr1, size_t len1, int* arr2, size_t len2) {
 }
 
 // 0 1 8 7 4 1 8 7 6
-void removeSubArr (int* arr1, size_t* len1, int* arr2, size_t len2) {
+void removeSubArr (int** arr1, size_t* len1, int* arr2, size_t len2) {
     int* subArr = NULL;
-    while ((subArr = findSubArr(arr1, *len1, arr2, len2))) {
+    while ((subArr = findSubArr(*arr1, *len1, arr2, len2))) {
         int* endSubArr = subArr + len2;
-        int* arr3 = concatArr(arr1, subArr - arr1, endSubArr, arr1 + (*len1) - endSubArr, len1);
-        // free(arr1);
-        arr1 = arr3;
-        printArr(arr1,*len1);
+        // Reusing concatArr function to join 2 arrays: before subArr and after
+        int* arr3 = concatArr(*arr1, subArr - *arr1, endSubArr, *arr1 + (*len1) - endSubArr, len1);
+        free(*arr1);
+        *arr1 = arr3;
     }
 }
 
-int main (void) {
+void perform () {
     size_t len1;
     size_t len2;
     scanf("%zu", &len1);
@@ -59,22 +66,23 @@ int main (void) {
     int* arr1 = malloc(sizeof(int) * len1);
     int* arr2 = malloc(sizeof(int) * len2);
 
+    nullCheck(arr1);
+    nullCheck(arr2);
+
     scanArr(arr1, len1);
     scanArr(arr2, len2);
 
     printArr(arr1, len1);
     printArr(arr2, len2);
 
-    // size_t len3;
-    // int* arr3 = concatArr(arr1, len1, arr2, len2, &len3);
-    // printArr(arr3, len3);
-    // int* subArr = findSubArr(arr1, len1, arr2, len2);
-    // if (subArr) {
-    //     printf("%p %d\n", subArr, *subArr);
-    // } else {
-    //     puts ("NULL");
-    // }
-    removeSubArr(arr1, &len1, arr2, len2);
+    removeSubArr(&arr1, &len1, arr2, len2);
     printArr(arr1, len1);
+
+    free(arr1);
+    free(arr2);
+}
+
+int main (void) {
+    perform();
     return 0;
 }
