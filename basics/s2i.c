82914974922f39d9ca4258bf7d7c49500f5d1f60
hitogava <hitogavax@yandex.ru>
2023-10-13 19:46:55 +0700

    Finally working version and implemented try-catch functionality by myself

--- basics/s2i.c
+++ basics/s2i.c
@@ -1,22 +1,27 @@
+#include <inttypes.h>
+#include <math.h>
+#include <setjmp.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <inttypes.h>
-#include <stdbool.h>
 #include <string.h>
-#include <setjmp.h>
-#include <math.h>
 
 typedef int32_t i32;
 typedef uint32_t ui32;
 
 #define STRING_FORMAT_ERROR 1
 #define STRING_BASE_ERROR 2
-#define NAD 3 // Not a digit
-#define print_i32(n) printf("%" PRId32 "\n",n)
+#define STRING_NAD_ERROR 3 // Not a digit
+
+#define try if (!state_val)
+#define catch(error) if (state_val == error)
+#define catch_other if (state_val)
+
+#define print_i32(n) printf("%" PRId32 "\n", n)
 
 /**
- * Magic number for convert 16 (or greater) base (A,B,...F) symbols into integer representation
- * E.g: A (65 in ASCII) - HEX_MAGIC_NUMBER = 10
+ * Magic number for convert 16 (or greater) base (A,B,...F) symbols into integer
+ * representation E.g: A (65 in ASCII) - HEX_MAGIC_NUMBER = 10
  */
 #define HEX_MAGIC_NUMBER 55
 
@@ -24,10 +29,10 @@ typedef uint32_t ui32;
  * If char contains non digit or letter of numeral system -> false else -> true
  */
 bool is_char_good(char ch, ui32* digit) {
-    if('0' <= ch && ch <= '9') {
+    if ('0' <= ch && ch <= '9') {
         *digit = (ch - '0');
         return true;
-    } else if('A' <= ch && ch <= 'F') { // boundaries are hardcoded
+    } else if ('A' <= ch && ch <= 'F') { // boundaries are hardcoded
         *digit = ch - HEX_MAGIC_NUMBER;
         return true;
     }
@@ -36,15 +41,20 @@ bool is_char_good(char ch, ui32* digit) {
 
 ui32 ensure_format(char ch, ui32 base, jmp_buf* state) {
     ui32 digit;
-    bool is_good = is_char_good(ch, &digit);
-    if (is_good && digit < base) {
+    bool isgood = is_char_good(ch, &digit);
+    if (isgood && digit < base) {
         return digit;
+    } else if (!isgood) {
+        longjmp(*state, STRING_NAD_ERROR);
+    } else if (digit >= base) {
+        longjmp(*state, STRING_BASE_ERROR);
+    } else {
+        longjmp(*state, STRING_FORMAT_ERROR);
     }
-    longjmp(*state, STRING_FORMAT_ERROR);
 }
 
 i32 s2i(const char* string, ui32 base, jmp_buf* state) {
-    if(base > 16) {
+    if (base > 16) {
         puts("Maximum base is 16");
         exit(0);
     }
@@ -55,23 +65,37 @@ i32 s2i(const char* string, ui32 base, jmp_buf* state) {
     ui32 sign = string_len > 0 && string[0] == '-' ? -1 : 1;
     for (int i = string_len - 1; i >= 0; i--) {
         char ch = string[i];
-        if (i == 0 && ch == '-') { continue; }
+        if (i == 0 && ch == '-') {
+            continue;
+        }
         ui32 digit = ensure_format(ch, base, state);
-        result += digit * (i32)(pow(base,power++));
+        result += digit * (i32)(pow(base, power++));
     }
     result *= sign;
     return result;
 }
 
-int main() {
+void example() {
     jmp_buf state;
     int state_val = setjmp(state);
-    if (state_val == STRING_FORMAT_ERROR ) {
-        puts("String format error");
+    try {
+        char* str = "-101010";
+        i32 str_to_int = s2i(str, 2, &state);
+        print_i32(str_to_int);
+    } catch (STRING_BASE_ERROR) {
+        puts("String contains digits that not fit base of numberical system");
+        exit(0);
+    } catch (STRING_NAD_ERROR) {
+        puts("String contains non digintal characters");
         exit(0);
     }
-    char* str = "123";
-    i32 str_to_int = s2i(str, 3, &state);
-    print_i32(str_to_int);
+    catch_other {
+        puts("Something went wrong");
+        exit(0);
+    }
+}
+
+int main() {
+    example();
     return 0;
 }
